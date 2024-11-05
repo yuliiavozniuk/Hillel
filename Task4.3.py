e64ed7aba1fc405242838e58b30bf3051ff5a5cc
yuliiavozniuk <zelenskaulia@gmail.com>
2024-11-05 21:11:42 +0200

    second commit

--- Task4.3.py
+++ Task4.3.py
@@ -1,6 +1,6 @@
 import random
 
-lst = [random.randint(1,10) for i in range(random.randint(3, 10))]
+lst = [random.randint(1,10) for _ in range(random.randint(3, 10))]
 print(lst)
 new_list = (lst[0], lst[2], lst[-2])
 print(list(new_list))
