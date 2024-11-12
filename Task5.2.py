6df70fb646da87b55b454c3b5cca68f87a498695
yuliiavozniuk <zelenskaulia@gmail.com>
2024-11-12 19:51:30 +0200

    first commit

---
+++ Task5.2.py
@@ -0,0 +1,22 @@
+while True:
+    first_digit = float(input("Enter first digit: "))
+    operator = input("Enter +, -, / or *: ")
+    second_digit = float(input("Enter second digit: "))
+    if operator == "+":
+        print(first_digit + second_digit)
+    elif operator == "-":(
+        print(first_digit - second_digit))
+    elif operator == "*":
+        print(first_digit * second_digit)
+    elif operator == "/":
+        if second_digit == 0:
+            print("The divisor cannot be equal to 0")
+        else:
+            print(first_digit / second_digit)
+    else:
+        print("Something went wrong!")
+
+    answer = input('Do you want to continue?')
+    if answer != 'yes' and answer != 'y':
+        print('Calculation is finished!')
+        break
