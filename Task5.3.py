265ceaa7c368877e07113a8e41cc1df5fdc8e7ff
yuliiavozniuk <zelenskaulia@gmail.com>
2024-11-12 21:47:44 +0200

    second commit

--- Task5.3.py
+++ Task5.3.py
@@ -1,6 +1,4 @@
-import string
-
 variable_1 = input('Print your variable: ')
 variable_2 = variable_1.title().replace(' ', '')
-variable_3 = ''.join(filter(str.isalnum, variable_2))
+variable_3 = ''.join(item for item in variable_2 if item.isalpha())
 print("#" + variable_3[:139])
