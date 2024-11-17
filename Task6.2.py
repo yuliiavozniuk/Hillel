654d0cc7bed56998af5a4c6dd76b9dc36df9d05e
yuliiavozniuk <zelenskaulia@gmail.com>
2024-11-17 23:40:26 +0200

    fourth commit

--- Task6.2.py
+++ Task6.2.py
@@ -8,6 +8,7 @@ rest_of_seconds %= (60 * 60)
 minutes = rest_of_seconds // 60
 seconds = rest_of_seconds % 60
 result = f"{days} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+
 if users_input > 8640000:
     print('The number is too long')
 elif users_input < 0:
