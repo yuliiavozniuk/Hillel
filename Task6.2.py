16ff92f23db966bdd3f1b79f719299bbfb05beb6
yuliiavozniuk <zelenskaulia@gmail.com>
2024-11-17 23:36:53 +0200

    third commit

--- Task6.2.py
+++ Task6.2.py
@@ -7,7 +7,10 @@ hours = rest_of_seconds // (60 * 60)
 rest_of_seconds %= (60 * 60)
 minutes = rest_of_seconds // 60
 seconds = rest_of_seconds % 60
-
 result = f"{days} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
-
-print(result)
+if users_input > 8640000:
+    print('The number is too long')
+elif users_input < 0:
+    print('The number is too small')
+else:
+    print(result)
