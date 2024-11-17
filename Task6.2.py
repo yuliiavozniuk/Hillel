b7cf936912bd1d940be77c018f86ae87642e315b
yuliiavozniuk <zelenskaulia@gmail.com>
2024-11-17 23:27:31 +0200

    second commit

---
+++ Task6.2.py
@@ -0,0 +1,13 @@
+users_input = int(input('Enter your number: '))
+
+seconds = users_input
+days = seconds // (24 * 60 * 60)
+rest_of_seconds = seconds % (24 * 60 * 60)
+hours = rest_of_seconds // (60 * 60)
+rest_of_seconds %= (60 * 60)
+minutes = rest_of_seconds // 60
+seconds = rest_of_seconds % 60
+
+result = f"{days} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}"
+
+print(result)
