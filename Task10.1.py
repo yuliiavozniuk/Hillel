994e6ea0fba9fad64d7f8f02e77ccfecc1ae3eae
yuliiavozniuk <zelenskaulia@gmail.com>
2024-11-27 20:05:20 +0200

    first commit

---
+++ Task10.1.py
@@ -0,0 +1,17 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    for _ in range(end):
+        yield begin
+        begin = func(begin)
+
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
