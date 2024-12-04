375f39311982d7c8b146e1b83210b83b14dafbac
yuliiavozniuk <zelenskaulia@gmail.com>
2024-12-04 18:24:25 +0200

    second commit

--- Task12.1.py
+++ Task12.1.py
@@ -1,12 +1,20 @@
 import codecs
-import re
 
 def delete_html_tags(html_file, result_file='cleaned.txt'):
       with codecs.open(html_file, 'r', 'utf-8') as file:
            html = file.read()
 
 
-      cleaned_text = re.sub(r'<.*?>', '', html)
+      cleaned_text = ''
+      symbols = False
+      for item in html:
+          if item == '<':
+              symbols = True
+          elif item == '>':
+              symbols = False
+          elif not symbols:
+              cleaned_text += item
+
       with codecs.open(result_file, 'w', 'utf-8') as new_file:
           new_file.write(cleaned_text.strip())
 
